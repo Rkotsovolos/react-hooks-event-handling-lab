be80d09a942825424e0dd768c142b0f622ed0f30
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-11-02 22:09:28 -0700

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function Keypad () {
+
+    function password() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <input onChange={password}type="password" />
+    );
+}
+
+export default Keypad;
